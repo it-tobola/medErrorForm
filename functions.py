f19f891f679bfd01e34e7e61c4bbbb00d798caa8
it-tobola <it@tobolainc.com>
2023-07-10 16:55:28 -0400

    Added visualiztion to the analyze tab2

--- functions.py
+++ functions.py
@@ -139,7 +139,7 @@ def viz_filters(site, individuals, grouping):
             filtered_errors = filtered_errors.append(row)
 
     if grouping == "Program":
-        st.write(filtered_errors.columns)
+        st.dataframe(filtered_errors)
         filtered_errors = filtered_errors.groupby(filtered_errors["Work Locations"]).count()
         return st.bar_chart(data=filtered_errors)
     elif grouping == "Service Recipient":
